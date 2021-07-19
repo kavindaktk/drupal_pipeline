1abd224f4a7dd775bb4c8d7e78f9d38a0081a064
kavindaktk <36921119+kavindaktk@users.noreply.github.com>
2021-07-20 01:07:36 +0530

    Update settings.php

--- package/sites/default/settings.php
+++ package/sites/default/settings.php
@@ -313,7 +313,7 @@ $settings['hash_salt'] = 'i57KN-UXcOpE3R83FozTMHUa7QqO9SYY4HeRbNgBfjvOE_NwEv_vzT
  * After finishing the upgrade, be sure to open this file again and change the
  * TRUE back to a FALSE!
  */
-$settings['update_free_access'] = FALSE;
+$settings['update_free_access'] = TRUE;
 
 /**
  * External access proxy settings:
